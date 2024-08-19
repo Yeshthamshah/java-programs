f155ba700b8d0fc084459f7586e81a9a0b1c3e83
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-08-19 21:33:55 +0530

    Update Averagevalueofarrayelements.java

--- Averagevalueofarrayelements.java
+++ Averagevalueofarrayelements.java
@@ -6,7 +6,7 @@ public class Averagevalueofarrayelements {
         int[] numbers = new int[]{20, 30, 25, 35, -16, 60, -100};
         int sum = 0;
         
-        // Use a for loop to iterate over the elements of the 'numbers' array.
+        // Use for loop to iterate over the elements of the 'numbers' array.
         for (int i = 0; i < numbers.length; i++) {
             
             sum = sum + numbers[i];
