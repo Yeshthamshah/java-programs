b174539af3ac7f3cf5084ec4074abc9d53135847
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-09-12 22:25:32 +0530

    Update and rename FindMinInLinearSearch.java to FindMinInLinearSearchh.java

--- FindMinInLinearSearchh.java
+++ FindMinInLinearSearchh.java
@@ -1,4 +1,4 @@
-public class FindMinInLinearSearch {
+public class FindMinInLinearSearchh {
     public static void main(String[] args) {
         int[] arr = {18, 12, 7, 3, 14, 28};
         System.out.println(min(arr));
