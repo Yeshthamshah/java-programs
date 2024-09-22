799e133b8af3cc7c32f10db16d26ac10eb2d8a4e
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-09-22 22:42:57 +0530

    Update LoopDemo.java

--- LoopDemo.java
+++ LoopDemo.java
@@ -1,7 +1,7 @@
 class LoopDemo {
 	public static void main(String args[])
 	{
-		for (int i = 1; i <= 5; i++)
+		for (int i = 1; i <= 5; i++) // for loop
 			System.out.println("Hello World");
 	}
 }
