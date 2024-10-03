baf86b3f8150659eda48e154c76c39b41e168961
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-10-03 09:40:47 +0530

    Create StaticVariables.java

---
+++ StaticVariables.java
@@ -0,0 +1,9 @@
+// Static variables
+import java.io.*;
+class StaticVariables {
+    // Declared static variable
+    public static String geek = "Yeshtham Shah";
+    public static void main(String[] args){
+        System.out.println("Geek Name is : " + StaticVariables.geek);
+    }
+}
