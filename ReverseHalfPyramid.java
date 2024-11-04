120bd673d46ed70a2318e58a3108d0b5465a6b14
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-11-04 18:24:38 +0530

    Create ReverseHalfPyramid.java

---
+++ ReverseHalfPyramid.java
@@ -0,0 +1,13 @@
+
+class ReverseHalfPyramid {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i=n;i<=n;i--){
+        for (int j=1;j<=i;j++){
+        System.out.print("*");
+        } 
+        System.out.println();
+    
+}
+}
+}
